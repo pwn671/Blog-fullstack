65f77ce39b6174bfd841af591bcf0b57e31ac18d
pwn671 <pwnsharma671@gmail.com>
2024-12-01 12:09:21 +0530

    Update server.js

--- server.js
+++ server.js
@@ -26,6 +26,15 @@ app.use("/api/v1/auth", require("./routes/userRoutes"));
  app.use("/api/v1/post", require('./routes/postRoutes'));
 //app.use("/api/v1/post", require("./routes/postRoutes"));
 
+
+//home
+app.get("/",(req, res)=>{
+ res.status(200).send({
+  ""success:true,
+  "msg":"Node Server Runn"
+ })
+})
+
 const PORT =  process.env.PORT || 8080
 
 
